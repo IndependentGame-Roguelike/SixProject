884185de66485096c4035880e65494be0a5c8d1a
huffy <tom-hu@hotmail.com>
2017-06-26 22:28:03 +0800

    add google sdk

--- SixProject/Assets/My/Scripts/Reward.cs
+++ SixProject/Assets/My/Scripts/Reward.cs
@@ -2,47 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Reward : MonoBehaviour {
-
-    public static Reward _instance;
-
-    private void Awake()
-    {
-        _instance = this;
-    }
-
-   
-
-    void Start()
+namespace My.Script
+{
+    public class Reward : MonoBehaviour
     {
 
-        DesSelf();
-    }
+        public static Reward _instance;
 
-    void Update()
-    {
-
-    }
-    public void Open()
-    {
-        this.gameObject.SetActive(true);
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Pieces")
+        private void Awake()
         {
-            collision.gameObject.transform.parent.gameObject.SetActive(false);
-            Debug.Log("Destroy1");
+            _instance = this;
+        }
+
+
+
+        void Start()
+        {
+
+            DesSelf();
+        }
+
+        void Update()
+        {
+
+        }
+        public void Open()
+        {
+            this.gameObject.SetActive(true);
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.tag == "Pieces")
+            {
+                collision.gameObject.transform.parent.gameObject.SetActive(false);
+                Debug.Log("Destroy1");
+            }
+        }
+
+
+        void DesSelf()
+        {
+            Debug.Log("GO");
+            GameObject.Destroy(GameObject.Find("RewardButton"));
+            GameObject.Destroy(gameObject, 4f);
         }
     }
-    
-
-    void DesSelf()
-    {
-        Debug.Log("GO");
-        GameObject.Destroy(GameObject.Find("RewardButton"));
-        GameObject.Destroy(gameObject, 4f);
-    }
-
 }
